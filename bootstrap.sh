67f0a63547c28dd20ccd0f09c617cb19c2e87885
Andrew N Leonard <anl@waimanalo.local>
2016-11-17 18:06:44 -0800

    Install Xcode, if needed

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if test xcode-select >&- && xpath=$(xcode-select --print-path) && test -d "$xpath" && test -x "$xpath" ; then
+    echo 'Xcode already configured'
+else
+    echo 'Select "Get Xcode" in the following prompt'
+    sleep 2
+    /usr/bin/xcode-select --install
+    echo 'Once install is completed, run "sudo git" to accept the Xcode'
+    echo 'license, and re-run this script'
+    exit
+fi
+
+
