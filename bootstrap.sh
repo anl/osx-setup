2a3e62021cd7638dba4e675ad26e4d8d164b1ba2
Andrew Leonard <andy@hurricane-ridge.com>
2016-11-17 18:33:14 -0800

    Complete Ansible virtualenv setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,3 +14,27 @@ fi
 
 # Configure git:
 #/usr/bin/git config --global --edit
+
+# Install Homebrew:
+if test -x /usr/local/bin/brew ; then
+    echo 'Homebrew already installed'
+else
+    /usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+if test -x /usr/local/bin/python ; then
+    echo 'Python already installed'
+else
+    /usr/local/bin/brew install python
+    /usr/local/bin/pip install --upgrade pip
+fi
+
+if test -x /usr/local/bin/virtualenv ; then
+    echo 'Virtualenv already installed'
+else
+    /usr/local/bin/pip install virtualenv
+fi
+
+mkdir -p ${HOME}/virtualenvs
+/usr/local/bin/virtualenv ${HOME}/virtualenvs/ansible
+${HOME}/virtualenvs/ansible/bin/pip install -r ansible-requirements.txt
